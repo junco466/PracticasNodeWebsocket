d34bf1c7972584a9816a6a19937671e59833151a
Juan Camilo Sepulveda <77352791+junco466@users.noreply.github.com>
2023-02-19 11:35:45 -0500

    Update key_uri.js

--- backendNode/database/key_uri.js
+++ backendNode/database/key_uri.js
@@ -4,4 +4,4 @@ module.exports = {
   db: `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
 }
 
-//mongodb+srv://shorlak:cojola51@cluster0.xn1mr.mongodb.net/?retryWrites=true&w=majority
+//mongodb+srv://user:password@cluster0.xn1mr.mongodb.net/?retryWrites=true&w=majority
